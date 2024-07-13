babc6cf90d6288f3682c45276a38ad91f3582991
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-07-13 15:03:14 -0400

    Create user.js

---
+++ user.js
@@ -0,0 +1,8 @@
+/* window control DE overrides */
+
+user_pref("OS.kde", true);
+user_pref("OS.gnome", false);
+user_pref("OS.mac", false);
+user_pref("OS.kde.themed", false);
+user_pref("OS.gnome.themed.i", false);
+user_pref("OS.gnome.themed.ii", false);
