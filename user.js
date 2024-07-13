f717b38882304ebc7b9298bb070e87265a531854
soulhotel <155501797+soulhotel@users.noreply.github.com>
2024-07-13 15:55:52 -0400

    Update user.js

--- user.js
+++ user.js
@@ -2,7 +2,3 @@
 
 user_pref("OS.kde", true);
 user_pref("OS.gnome", false);
-user_pref("OS.mac", false);
-user_pref("OS.kde.themed", false);
-user_pref("OS.gnome.themed.i", false);
-user_pref("OS.gnome.themed.ii", false);
